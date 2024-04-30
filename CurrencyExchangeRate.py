0ab7dce996466d22dbf3ffd3b94991c2a01eeda9
BartłomiejGazduslki <heluios05@gmail.com>
2024-04-30 22:36:59 +0200

    dodana funkcja printujaca tabele, funkcje przyjmuja zmienne danego typu

--- CurrencyExchangeRate.py
+++ CurrencyExchangeRate.py
@@ -4,24 +4,30 @@ import requests
 import sys
 import json
 
-def CurrentExangeRate(CurrencyName):
+# assumption: length of columnOne and columnTwo is equal
+def TablePrint(headlineOne:str, headlineTwo:str, columnOne:list, columnTwo:list):
+    if len(columnOne) != len(columnTwo):
+        # if that statement is true, its a critital error
+        sys.exit -1
+
+    print(headlineOne+"\t"+"\t"+headlineTwo)
+    for iterator in range(len(columnOne)):
+        print(str(columnOne[iterator])+"\t"+str(columnTwo[iterator]))
+
+def CurrentExangeRate(CurrencyName:str):
     return float((json.loads(requests.get(f"http://api.nbp.pl/api/exchangerates/rates/A/{CurrencyName}/").text))["rates"][0]['mid'])
 
-def Last5DaysCurrentExangeRate(CurrencyName):
+def Last5DaysCurrentExangeRate(CurrencyName:str):
     lastDays = 5
     url = f"http://api.nbp.pl/api/exchangerates/rates/A/{CurrencyName}/last/{lastDays}/"
     fiveDaysCurrency = json.loads(requests.get(url).text)["rates"]
-    print(fiveDaysCurrency)
-    """
+    
+    dates = []
     currencyRates = []
     for iterator in fiveDaysCurrency:
         currencyRates.append(iterator["mid"])
-    return currencyRates
-    """
-    
-
-
-
+        dates.append(iterator["effectiveDate"])
+    TablePrint("Dates", "CurrencyRates", dates, currencyRates)
 
 if __name__ == "__main__":
     # todo: pobieranie nazwy waluty ze standardowego wejscia
@@ -30,4 +36,4 @@ if __name__ == "__main__":
     currentResponse = CurrentExangeRate(currencyName)
     print(currentResponse)
     """
-    print(Last5DaysCurrentExangeRate("USD"))
+    Last5DaysCurrentExangeRate("USD")

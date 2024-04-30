fab64a72b382cd5989bca89167180c0b587e1561
BartłomiejGazduslki <heluios05@gmail.com>
2024-04-30 21:35:41 +0200

    niedokonczona wersja alpha skryptu

---
+++ CurrencyExchangeRate.sh
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import requests
+import sys
+import json
+
+def CurrentExangeRate(CurrencyName):
+    return float((json.loads(requests.get(f"http://api.nbp.pl/api/exchangerates/rates/A/{CurrencyName}/").text))["rates"][0]['mid'])
+
+def Last5DaysCurrentExangeRate(CurrencyName):
+    lastDays = 5
+    url = f"http://api.nbp.pl/api/exchangerates/rates/A/{CurrencyName}/last/{lastDays}/"
+    fiveDaysCurrency = json.loads(requests.get(url).text)["rates"]
+    
+    currencyRates = []
+    for iterator in fiveDaysCurrency:
+        currencyRates.append(iterator["mid"])
+    return currencyRates
+
+    
+
+
+
+
+if __name__ == "__main__":
+    # todo: pobieranie nazwy waluty ze standardowego wejscia
+    """
+    currencyName = "USD"
+    currentResponse = CurrentExangeRate(currencyName)
+    print(currentResponse)
+    """
+    print(Last5DaysCurrentExangeRate("USD"))

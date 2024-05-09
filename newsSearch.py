311333c0985dcc9c3915fa09c4c55735c4b61eee
BartłomiejGazduslki <heluios05@gmail.com>
2024-05-09 17:33:59 +0200

    dodane komentarze odnosnie funkcjonowania programu

--- newsSearch.py
+++ newsSearch.py
@@ -5,6 +5,7 @@ import requests
 import json
 import argparse
 
+# funcion call help of that script in cmd
 def Help():
     parser = argparse.ArgumentParser(
         prog = 'newsSearch.py',
@@ -15,10 +16,18 @@ def Help():
     parser.add_argument('keyword')
     args=parser.parse_args()
 
-def APIcall(APIkey:str, keyword:str):
-    url = f"https://newsapi.org/v2/everything?q={keyword}&apiKey={APIkey}"
-    return json.loads(requests.get(url).text)
 
+# funcion send API request and return an API response as dict
+def APIcall(APIkey:str, keyword:str) -> dict:
+    try:
+        url = f"https://newsapi.org/v2/everything?q={keyword}&apiKey={APIkey}"
+        return json.loads(requests.get(url).text)
+    except:
+        # if something goes wrong then script exits with error code -1
+        sys.exit(-1)
+
+
+# function interpets an API response
 def APIresponseInterpreter(APIkey:str, keyword:str):
     try:
         APIresponse = APIcall(APIkey, keyword)
@@ -27,10 +36,14 @@ def APIresponseInterpreter(APIkey:str, keyword:str):
         if APIresponse["status"] != "ok":
             sys.exit(-1)
     
+        # printing a header
         print(f"lists of articles about {keyword}:\n")
+
+        # loop lists an articles
         for iterator in range(len(APIresponse["articles"])):
             print("author: "+str(APIresponse["articles"][iterator]["author"])+"\ntitle: "+APIresponse["articles"][iterator]["title"]+"\nurl: "+APIresponse["articles"][iterator]["url"]+"\n")
     except:
+        # if something goes wrong, program exits with error code -1
         sys.exit(-1)
 
 # sys.argv[1] - API key

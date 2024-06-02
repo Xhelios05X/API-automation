72ff4c821791c3815e97898b4b02d7f1ec273d62
BartłomiejGazduslki <heluios05@gmail.com>
2024-06-02 23:07:42 +0200

    README.md and newsSearch.py were changed

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ----------------------------------
 eng. below
 
-Repozytorium zawiera skrypty odwołujące się do różncy API: news-api, api NBP oraz Shodan api
+Repozytorium zawiera skrypty automatyzujące pewne zadania odwołujące się do różnych API: news-api, api NBP oraz Shodan api.
 
 ## CurrencyExchangeRate.py
 
@@ -24,4 +24,45 @@ alternatywnie dla systemu Linux:
 chmod +x ./CurrencyExchangeRate.py
 
 ./CurrencyExchangeRate.py [-h] currency
+```
+
+## newsSearch.py
+
+Skrypt pobiera słowo kluczowe ('keyword') na temat którego użytkownik chciałby otrzymać infromacje. **Warunkiem działania programu jest podanie klucza API dla serwisu [newsAPI](https://newsapi.org/)**:
+Account > API key
+
+### Użycie:
+
+za pomocą interpretera python3:
+
+```shell
+python3 newsSearch.py [-h] APIkey keyword
+```
+
+alternatywnie dla systemu Linux:
+
+```shell
+chmod +x ./newsSearch.py
+
+./newsSearch.py [-h] APIkey keyword
+```
+
+## shodanSearch.py
+
+Dzięki skryptowi można poznać aktywne porty określonego hosta. Program używa API Shodan do wykrywania aktywnych portów. **Warunkiem działania skryptu jest podanie klucza API dla serwisu [Shodan](https://developer.shodan.io/?language=en)**: 
+login > logowanie > prawy górny róg > `Show API key`
+
+### Użycie:
+
+za pomocą interpretera python3:
+
+```shell
+python3 shodanSearch.py [-h] APIkey hostIP
+```
+
+alternatywnie dla systemu Linux:
+```shell
+chmod +x ./shodanSearch.py
+
+./shodanSearch.py [-h] APIkey hostIP
 ```
--- newsSearch.py
+++ newsSearch.py
@@ -12,10 +12,16 @@ def Help():
         description = "Script process a news API api response and return lists of articles about typed keyword",
     )
 
+    parser.add_argument("-f", "--file", action="store_true")
     parser.add_argument("APIkey")
     parser.add_argument("keyword")
     args=parser.parse_args()
+    return args
 
+def OutputToFile(APIresponse):
+    #   ToDo:
+    #   function output file
+    pass
 
 # funcion send API request and return an API response as dict
 def APIcall(APIkey:str, keyword:str) -> dict:
@@ -28,22 +34,25 @@ def APIcall(APIkey:str, keyword:str) -> dict:
 
 
 # function interpets an API response
-def APIresponseInterpreter(APIkey:str, keyword:str):
+def APIresponseInterpreter(APIkey:str, keyword:str, isFile = False):
     try:
         APIresponse = APIcall(APIkey, keyword)
     
         # if API response isn't ok, then script exit with error code -1
         if APIresponse["status"] != "ok":
             sys.exit(-1)
-    
-        # printing a header
-        print(f"lists of articles about {keyword}:\n")
 
-        # loop lists an articles
-        for iterator in range(len(APIresponse["articles"])):
-            print("author: "+str(APIresponse["articles"][iterator]["author"])+
-                  "\ntitle: "+APIresponse["articles"][iterator]["title"]+
-                  "\nurl: "+APIresponse["articles"][iterator]["url"]+"\n")
+        if help.file:
+            OutputToFile(APIresponse)
+        else:
+            # printing a header
+            print(f"lists of articles about {keyword}:\n")
+
+            # loop lists an articles
+            for iterator in range(len(APIresponse["articles"])):
+                print("author: "+str(APIresponse["articles"][iterator]["author"])+
+                    "\ntitle: "+APIresponse["articles"][iterator]["title"]+
+                    "\nurl: "+APIresponse["articles"][iterator]["url"]+"\n")
     except:
         # if something goes wrong, program exits with error code -1
         sys.exit(-1)
@@ -52,8 +61,8 @@ def APIresponseInterpreter(APIkey:str, keyword:str):
 # sys.argv[1] - API key
 # sys.argv[2] - keyword of news
 if __name__ == "__main__":
-    Help()
+    help = Help()
 
     APIkey = sys.argv[1]
     keyword = sys.argv[2]
-    APIresponseInterpreter(APIkey, keyword)
+    APIresponseInterpreter(APIkey, keyword, help.)
